bdeaf08ac14067b8744693e4884679edc8950d07
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-01-11 09:52:17 +0100

    update rsync

--- data/rsync_data.sh
+++ data/rsync_data.sh
@@ -6,3 +6,13 @@ REMOTE=europa  # The name of the remote machine in your .ssh/config file
 REMOTE_DIR="~/Git/ScienceProjectTemplate.jl/data/raw"
 
 rsync -avz $REMOTE:$REMOTE_DIR ./
+
+
+## TWO WAYS SYNC 
+## Notice that the data is not deleted from the local machine
+REMOTE=bidsa  # The name of the remote machine in your .ssh/config file
+REMOTE_DIR="~/Git/Benchmarks_SAT/benchmarks"
+rsync -avz $REMOTE:$REMOTE_DIR ../ --include='*.csv' --include='*/' --exclude='*'
+echo "Sync from remote machine $REMOTE to local machine terminated."
+read -n 1 -s -r -p "--> Press any key to proceed with sync from local to remote or kill with Crl+C."
+rsync -avz --delete ./ $REMOTE:$REMOTE_DIR --include='*.csv' --include='*/' --exclude='*'
